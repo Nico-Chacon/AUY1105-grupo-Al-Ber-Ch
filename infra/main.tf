71b63ea2755eeac2ce60247e9db66253efd9a00c
Giancarlo-Berardinucci <gi.berardinucci@duocuc.cl>
2026-04-29 00:35:39 -0400

    agregado Internet Gatewat y Route Table configurada

--- infra/main.tf
+++ infra/main.tf
@@ -43,3 +43,21 @@ resource "aws_instance" "AUY1105-duocapp-ec2" {
     Name = "AUY1105-duocapp-ec2"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.AUY1105-duocapp-vpc.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.AUY1105-duocapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.AUY1105-duocapp-subnet.id
+  route_table_id = aws_route_table.rt.id
+}
